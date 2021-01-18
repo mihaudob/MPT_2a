de336953d77a1d5d9361b9885a02f707ed6ac430
kotmich <kotmich97@gmail.com>
2021-01-18 12:22:36 -0800

    Attack fix

--- simple_switch_stp.py
+++ simple_switch_stp.py
@@ -7,8 +7,16 @@ from ryu.lib import dpid as dpid_lib
 from ryu.lib import stplib
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
+from ryu.lib.packet import dhcp
+from ryu.lib.packet import ipv4
+from datetime import timedelta
+from datetime import time
 from controller import simple_switch_13
 
+DHCP_COUNTER = 0
+DHCP_LIMIT = 5
+DHCP_INTERVAL = timedelta(seconds=5)
+drop_checker = {}
 
 class SimpleSwitch13(simple_switch_13.SimpleSwitch13):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
@@ -82,6 +90,55 @@ class SimpleSwitch13(simple_switch_13.SimpleSwitch13):
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
                                   in_port=in_port, actions=actions, data=data)
         datapath.send_msg(out)
+
+        pkt_dhcp = pkt.get_protocols(dhcp.dhcp)
+        if not pkt_dhcp:
+            return
+        else:
+            drop_checker[in_port] = False
+            self._handle_dhcp(datapath, in_port, pkt)
+
+        if drop_checker[in_port] == True:
+            self.delete_flow(datapath)
+        else:
+            self.logger.info("DROP_CHECKER == FALSE")
+        return
+
+    def get_state(self, pkt_dhcp):
+        dhcp_state = ord(
+            [opt for opt in pkt_dhcp.options.option_list if opt.tag == 53][0].value)
+        if dhcp_state == 1:
+            state = 'DHCPDISCOVER'
+        elif dhcp_state == 2:
+            state = 'DHCPOFFER'
+        elif dhcp_state == 3:
+            state = 'DHCPREQUEST'
+        elif dhcp_state == 5:
+            state = 'DHCPACK'
+        return state
+
+    def _handle_dhcp(self, datapath, port, pkt):
+        global DHCP_COUNTER
+        global DHCP_LIMIT
+        global DHCP_INTERVAL
+
+        pkt_dhcp = pkt.get_protocols(dhcp.dhcp)[0]
+        dhcp_state = self.get_state(pkt_dhcp)
+        self.logger.info("NEW DHCP %s PACKET RECEIVED: %s" %
+                         (dhcp_state, pkt_dhcp))
+        if dhcp_state == 'DHCPREQUEST':
+            DHCP_COUNTER += 1
+            if DHCP_COUNTER == 1:
+                first_dhcp = time.time()
+            elif DHCP_COUNTER == DHCP_LIMIT:
+                last_dhcp = time.time()
+                if last_dhcp - first_dhcp < DHCP_INTERVAL:
+                    self.logger.info("DHCP STARVATION ATTACK DISCOVERED ON PORT: %s", port)
+                    drop_checker[port] = True
+            else:
+                pass
+        else:
+            return
 
     @set_ev_cls(stplib.EventTopologyChange, MAIN_DISPATCHER)
     def _topology_change_handler(self, ev):
